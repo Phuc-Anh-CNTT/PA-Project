913b0f5fdcd8614f97c833cd358536a7d098dce9
ndyduc <ndyduc0211@gmail.com>
2025-12-10 13:09:15 +0700

    stop zns DGCL

--- backend/app/api/routes/API_Caresoft.py
+++ backend/app/api/routes/API_Caresoft.py
@@ -58,8 +58,8 @@ async def bao_nhan_bh():
     print(f"bao nhan BH luc: {datetime.now()}")
     await asyncio.gather(
         call_api("baohanh"),
-        call_api("kscl_banhang"),
-        call_api("kscl_baohanh")
+        # call_api("kscl_banhang"),
+        # call_api("kscl_baohanh")
     )
 
 
